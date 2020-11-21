4e968debd05430a4ad6822f3b3ae8ff43baa4109
Attila Dekany <attila.dekany@abiscon-sas.com>
2020-11-21 12:58:09 +0100

    Set card-number-selector visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public isCardNumberSelectorVisible: boolean = false;
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((e) => {
+        if (e['url']?.includes('game-page')) {
+          this.isCardNumberSelectorVisible = true;
+        } else {
+          this.isCardNumberSelectorVisible = false;
+        }
+      });
+  }
   title = 'angular-memory-game-app';
 }
